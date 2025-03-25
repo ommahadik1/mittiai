d51a553ee7a707556c8a3ac0eaaddcdbe6520916
Om Mahadik <ommahadik245@gmail.com>
2025-03-25 23:27:53 +0530

    first commit

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -10,7 +10,7 @@ function Navbar() {
             <img
               src="/mitti-logo.jpeg"
               alt="Mitti Logo"
-              className="h-52 w-auto"
+              className="h-80 w-auto"
             />
           </div>
           <nav className="flex">
@@ -32,7 +32,7 @@ function Navbar() {
           </nav>
         </div>
       </header>
-      <div className="h-40"></div>
+      <div className="h-64"></div>
     </>
   );
 }
